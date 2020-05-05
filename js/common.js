7c8f9f9c0514c0ea174ce039eec8ac76bbbd06dd
jeonaryeon <aryeon96@naver.com>
2020-05-05 17:00:38 +0900

    js
    js test

--- js/common.js
+++ js/common.js
@@ -41,8 +41,8 @@ $(function(){
   function notice(){
     $('#notice tbody').stop().animate({top:-50},function(){
       $('#notice tbody tr:first').appendTo('#notice tbody');
-    },100);
-    $('#notice tbody').css({top:0});
+    },50);
+    $('#notice tbody').stop().animate({top:0});
   }
   setInterval(notice,3000);
 });
