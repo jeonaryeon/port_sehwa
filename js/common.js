c98f2eaff90ddd881f8bce7b90b7541e63541d0f
jeonaryeon <aryeon96@naver.com>
2020-05-09 15:13:07 +0900

    notice js test
    notice js test

--- js/common.js
+++ js/common.js
@@ -49,8 +49,8 @@ $(function(){
                                         
   /* #notice */
   function notice(){
-    $('#notice tbody').stop().animate({top:-40},function(){
-      $('#notice tbody tr:first').appendTo('#notice tbody');
+    $('#notice tbody').stop().animate({top:-40},300,function(){
+      $('#notice tbody tr:first').appendTo('#notce tbody');
       $('#notice tbody').css({top:0});
     });
   }
