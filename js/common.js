80d4cb3cdf0fbf48ee169d81d695be5038ff44f6
jeonaryeon <aryeon96@naver.com>
2020-05-09 15:19:46 +0900

    js test
    js test

--- js/common.js
+++ js/common.js
@@ -40,7 +40,7 @@ $(function(){
   
   /* #banner */
   function slides(){
-    $('#banner .banner').stop().animate({left:'-50%'},function(){
+    $('#banner .banner').stop().animate({left:'-50%'},500,function(){
       $('#banner .banner a:first').appendTo('#banner .banner');
       $('#banner .banner').css({left:0});
     });
