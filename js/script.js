c5469f28f3c1a0274abdee005686af65a05f5d36
jeonaryeon <aryeon96@naver.com>
2020-05-31 17:43:07 +0900

    sehwa_200531
    sehwa_200531

--- js/script.js
+++ js/script.js
@@ -27,5 +27,27 @@ $(function(){
       $('.found_img figcaption').css({display:'none'});
     }
   });
+  
+  
+  /* #museum_shop */
+  $('.shop_tabbox li').on('click',function(){
+    $(this).addClass('on').siblings().removeClass('on');
+    
+    if($('.shop_tabbox .all').hasClass('on')){
+      $('.shop .all').css({display:'block'});
+      $('#museum_shop .shop li').css({margin:'0 0 30px'});
+      $('#museum_shop .shop li:nth-of-type(3n+2)').css({margin:'0 5% 30px'});
+    }else if($('.shop_tabbox .sma').hasClass('on')){
+      $('.shop .other').css({display:'none'});
+      $('.shop .sma').css({display:'block'});
+      $('#museum_shop .shop li').css({margin:'0 0 30px'});
+      $('.shop .m').css({margin:'0 5% 30px'});
+    }else{
+      $('.shop .sma').css({display:'none'});
+      $('.shop .other').css({display:'block'});
+      $('#museum_shop .shop li').css({margin:'0 0 30px'});
+      $('.shop .other:nth-of-type(3n+1)').css({margin:'0 5% 30px'});
+    }
+  });
 });
 
